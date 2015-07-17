ff51918a3fd77b65f52731d12de5ad3cfe3ea77d
Ari Lamstein <arilamstein@gmail.com>
2015-07-17 13:23:52 -0700

    initial commit
    select an admin1 map from a list of country names.

---
+++ server.R
@@ -0,0 +1,22 @@
+library(shiny)
+library(choroplethr)
+library(choroplethrAdmin1)
+library(mapproj)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+
+  output$map = renderPlot({
+
+    data(admin1.regions)
+    country = input$country
+    regions = unique(admin1.regions[admin1.regions$country == country, "region"])
+    df = data.frame(region=regions, value=1:length(regions))
+                         
+    admin1_choropleth(country.name = country, 
+                      df           = df,
+                      num_colors   = 1)
+
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(choroplethr)
+library(choroplethrAdmin1)
+
+data(admin1.regions)
+countries = unique(admin1.regions$country)
+
+shinyUI(fluidPage(
+
+  titlePanel("Administrative Level 1 Map and Projection Demo"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("country", "Country:", choices = countries, selected = "japan")
+    ),
+
+    mainPanel(
+      plotOutput("map")
+    )
+  )
+))
